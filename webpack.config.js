15c9a6c4eab7c1b5cdc6a56ab2f5b466b1976990
Michael Jonathan Lee <code.mjlee@gmail.com>
2017-11-17 19:47:35 +0100

    Auto generate HTML file with bundles.

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,12 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
 module.exports = {
     entry: './src/index.js',
     output: {
-        path: __dirname + '/bundles',
+        path: __dirname + '/dist',
         filename: 'bundle.js',
     },
+    plugins: [
+        new HtmlWebpackPlugin({template: __dirname + '/index.html'}),
+    ],
 }
