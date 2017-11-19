1ff7615a6eab89967617e13702ba12d86541b948
Michael Jonathan Lee <code.mjlee@gmail.com>
2017-11-19 19:01:42 +0100

    Basic React setup.

--- webpack.config.js
+++ webpack.config.js
@@ -2,10 +2,19 @@ const HtmlWebpackPlugin = require('html-webpack-plugin')
 const CleanWebpackPlugin = require('clean-webpack-plugin')
 
 module.exports = {
-    entry: './src/index.js',
+    entry: './src/index.jsx',
     output: {
         path: __dirname + '/dist',
         filename: 'bundle.js',
+    },
+    module: {
+        rules: [
+            {
+                test: /\.jsx?$/,
+                use: 'babel-loader',
+                exclude: /node_modules/
+            },
+        ],
     },
     plugins: [
         new HtmlWebpackPlugin({template: __dirname + '/index.html'}),
