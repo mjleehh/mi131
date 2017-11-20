91aad3124fd8f8113c12bc4b84e574afefa641ee
Michael Jonathan Lee <code.mjlee@gmail.com>
2017-11-20 14:11:01 +0100

    Create a stateful component.

--- src/index.jsx
+++ src/index.jsx
@@ -1,9 +1,38 @@
 import React from 'react'
 import ReactDom from 'react-dom'
 
-function RootComponent(props) {
-    console.log('render root component')
-    return <h1>Root Component Body</h1>
+class NameInput extends React.Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {name: '', surname: ''}
+
+        this.handleNameChange = event => this.setState({name: event.target.value})
+        this.handleSurnameChange = event => this.setState({surname: event.target.value})
+        this.handleSubmit = event => {
+            const {onChange} = this.props
+            if (onChange) {
+                onChange(Object.assign({}, this.state))
+            }
+            event.preventDefault()
+        }
+    }
+
+    render() {
+        return <form onSubmit={this.handleSubmit}>
+            <div>
+                <label>name</label>
+                <input type="text" value={this.state.name} onChange={this.handleNameChange}/>
+            </div>
+            <div>
+                <label>surname</label>
+                <input type="text" value={this.state.surname} onChange={this.handleSurnameChange}/>
+            </div>
+            <input type="submit" value="Ok" />
+        </form>
+    }
 }
 
-ReactDom.render(<RootComponent />, document.getElementById("main"))
+const handleNameInput = name => console.log('name set to:', name.name, name.surname)
+
+ReactDom.render(<NameInput onChange={handleNameInput} />, document.getElementById("main"))
