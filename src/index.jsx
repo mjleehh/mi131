c3701eea4698904f72e0e789d0fbd0b0874e2d94
Michael Jonathan Lee <code.mjlee@gmail.com>
2017-11-20 12:51:39 +0100

    Create stateless component.

--- src/index.jsx
+++ src/index.jsx
@@ -1,12 +1,9 @@
 import React from 'react'
 import ReactDom from 'react-dom'
 
-class RootComponent extends React.Component {
-    render() {
-        console.log('render root component')
-        return <h1>Root Component Body</h1>
-    }
+function RootComponent(props) {
+    console.log('render root component')
+    return <h1>Root Component Body</h1>
 }
-
 
 ReactDom.render(<RootComponent />, document.getElementById("main"))
