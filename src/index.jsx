1bf4303ea5e7bf7c5fd057fc9b44d21a48023d2a
Michael Jonathan Lee <code.mjlee@gmail.com>
2017-11-21 14:19:58 +0100

    Create a list with keys.

--- src/index.jsx
+++ src/index.jsx
@@ -1,38 +1,12 @@
 import React from 'react'
 import ReactDom from 'react-dom'
 
-class NameInput extends React.Component {
-    constructor(props) {
-        super(props)
+const names = [
+    'phil',
+    'pete',
+    'bob'
+]
 
-        this.state = {name: '', surname: ''}
+const renderedNames = names.map(name => <li key={name}>{name}</li>)
 
-        this.handleNameChange = event => this.setState({name: event.target.value})
-        this.handleSurnameChange = event => this.setState({surname: event.target.value})
-        this.handleSubmit = event => {
-            const {onChange} = this.props
-            if (onChange) {
-                onChange(Object.assign({}, this.state))
-            }
-            event.preventDefault()
-        }
-    }
-
-    render() {
-        return <form onSubmit={this.handleSubmit}>
-            <div>
-                <label>name</label>
-                <input type="text" value={this.state.name} onChange={this.handleNameChange}/>
-            </div>
-            <div>
-                <label>surname</label>
-                <input type="text" value={this.state.surname} onChange={this.handleSurnameChange}/>
-            </div>
-            <input type="submit" value="Ok" />
-        </form>
-    }
-}
-
-const handleNameInput = name => console.log('name set to:', name.name, name.surname)
-
-ReactDom.render(<NameInput onChange={handleNameInput} />, document.getElementById("main"))
+ReactDom.render(<ol>{renderedNames}</ol>, document.getElementById("main"))
