a43acecd7079f7dc6336e342120d42243fc9559a
Michael Jonathan Lee <code.mjlee@gmail.com>
2017-11-27 16:51:18 +0100

    Added redux store and set up redux dev tools.

--- src/index.jsx
+++ src/index.jsx
@@ -1,8 +1,23 @@
 import ReactDom from 'react-dom'
 import React from 'react'
+import {createStore, compose} from 'redux';
+import {Provider} from 'react-redux'
+
+import reducer from './reducer'
 
 import App from './App'
 
 import './style.css'
 
-ReactDom.render(<App />, document.getElementById('main'))
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+const enhancer = composeEnhancers()
+
+const store = createStore(reducer, enhancer)
+
+ReactDom.render(
+    <Provider store={store}>
+        <App />
+    </Provider>,
+    document.getElementById('main'))
+
---
+++ src/reducer.js
@@ -0,0 +1,11 @@
+function initialState() {
+    return {
+        number: '0',
+        acc: null,
+    }
+}
+
+
+export default function reducer(state = initialState(), action) {
+    return state
+}
